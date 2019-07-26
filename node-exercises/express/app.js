d77d560caea88e2d832153bc9e5f4f8e9d221fc4
Gurmehar <50474292+thedovesscar@users.noreply.github.com>
2019-07-25 18:37:57 -0700

    first couple videos from Node.js section

--- node-exercises/express/app.js
+++ node-exercises/express/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/hello.txt', function(req, res){
-  res.send('Hello World');
+  res.send('Hello Bruh');
 });
 
 var server = app.listen(8080, function() {
