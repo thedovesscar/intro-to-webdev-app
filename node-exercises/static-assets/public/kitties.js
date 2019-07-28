cc555a1508f758615a796d47d04c85dd84becae9
Gurmehar <50474292+thedovesscar@users.noreply.github.com>
2019-07-27 17:05:43 -0700

    Covered Static Assets section with Kitten Pics webapp

--- node-exercises/static-assets/public/kitties.js
+++ node-exercises/static-assets/public/kitties.js
@@ -1,5 +1,6 @@
 $('button').click(function() {
   var x = Math.round(Math.random() * 500);
   var y = Math.round(Math.random() * 500);
+  $('button').text("Press Me Again!");
   $('div').append('<img src="http://placekitten.com/' + x + '/' + y + '">');
 });
