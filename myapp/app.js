6b72199f3a948bea29fcf71e5e9891dcb703a119
Gurmehar <50474292+thedovesscar@users.noreply.github.com>
2019-08-01 01:11:14 -0700

    latenight Jul31'19- Trying to work out Server side of Project along with Client- stalled here a bit

--- myapp/app.js
+++ myapp/app.js
@@ -1,10 +1,29 @@
 var express = require('express');
 var app = express();
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+var tweets = [
+	{text: "Hai", time: new Date().getTime() - 12300},
+	{text: "Type something up there, DUH!", time: new Date().getTime() - 5000},
+	{text: "Welcome to This WebApp!", time: new Date().getTime()},
+];
+
 app.use(express.static(__dirname + '/public'));
+
+
+app.get('/ajax', function(request, response) {
+	response.type('json');
+	response.end(JSON.stringify({tweets:tweets}))
+});
+
+app.post('/ajax', function (request, response) {
+	var newTweet = {text: request.body.tweet, time: new Date().getTime()};
+	tweets.push(newTweet);
+	response.type('json');
+	response.end(JSON.stringify(newTweet));
+});
+
 
 
 var server = app.listen(8080);
--- myapp/public/tweet.js
+++ myapp/public/tweet.js
@@ -1,35 +1,36 @@
-var $tweetbtn = $("#tweet-button");
+$.ajax({
+	type: "GET",
+	url:"/ajax",
+	success: function(data) {
+		for (var i = 0; i < data.tweets.length; i++) {
+			prependNewTweet(data.tweets[i]);
+		}
+	}
+});
 
-$tweetbtn.click( function() {
+function prependNewTweet(data) {
+
+	var newTweet = "<div class='tweet-container'>" + 
+		"<div class='tweet-time'>" + new Date(data.time).toLocaleSTring() + "</div>" +
+		"<div class='tweet-body'>" + data.text + "</div>" +
+		"</div>";
+	console.log("here");
+	$("#tweets-section").append(newTweet);
+	console.log("here");
+};
+
+
+$('#tweet-button').click( function() {
+	console.log("here");
 	$.ajax({
 		type: "POST",
 		url: "/ajax",
 		contentType: 'application/json',
 		data: JSON.stringify({tweet: $('#new-tweet').val()}),
 		success: function(data) {
-			prependNewTweet(data);
-			$(".new-tweet-input").val("");
+			prependNewTweet(data.tweets[1]);
+			$("#new-tweet").text("");
 		}
 	})
+	console.log("here");
 });
-
-// $ajax({
-// 	type: "GET",
-// 	url:"/ajax",
-// 	success: function(data) {
-// 		for (var i = 0; i < data.tweets.length; i++) {
-// 			prependNewTweet(data.tweets[i]);
-// 		}
-// 	}
-// });
-
-function prependNewTweet(tweet) {
-
-	var newTweet = "<div class='tweet-container'>" + 
-		"<div class='tweet-time'>" + new Date(tweet.time).toLocaleSTring() + "</div>" +
-		"<div class='tweet-body'>" + tweet.text + "</div>" +
-		"</div>";
-
-	$("#tweets-section").prepend(newTweet);
-}
-
