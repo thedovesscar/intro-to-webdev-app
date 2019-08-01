dd3ce651e749ad129e9fee24de72b0bbfe2f1780
Gurmehar <50474292+thedovesscar@users.noreply.github.com>
2019-07-31 23:32:53 -0700

    postJul31'19 evening - Completed all of Client Side of Final Project

--- myapp/public/tweet.js
+++ myapp/public/tweet.js
@@ -4,13 +4,13 @@ $tweetbtn.click( function() {
 	$.ajax({
 		type: "POST",
 		url: "/ajax",
-		
+		contentType: 'application/json',
+		data: JSON.stringify({tweet: $('#new-tweet').val()}),
+		success: function(data) {
+			prependNewTweet(data);
+			$(".new-tweet-input").val("");
+		}
 	})
-
-
-
-	$(".new-tweet-input").val("");
-
 });
 
 // $ajax({
@@ -18,12 +18,12 @@ $tweetbtn.click( function() {
 // 	url:"/ajax",
 // 	success: function(data) {
 // 		for (var i = 0; i < data.tweets.length; i++) {
-// 			appendNewTweet(data.tweets[i]);
+// 			prependNewTweet(data.tweets[i]);
 // 		}
 // 	}
 // });
 
-function appendNewTweet(tweet) {
+function prependNewTweet(tweet) {
 
 	var newTweet = "<div class='tweet-container'>" + 
 		"<div class='tweet-time'>" + new Date(tweet.time).toLocaleSTring() + "</div>" +
